9b992fcf239749920031de1f28962dc5f29c65f7
Ana Carolina Costa César <anaipva@gmail.com>
2025-07-10 17:36:28 -0300

    #32 #33 #34 #5 ヽ(✿ﾟ▽ﾟ)ノ Minigame do restaurante e créditos
    Co-authored-by: PabloGJBS <plabo.jbs@gmail.com>

--- GameOver/Won.gd
+++ GameOver/Won.gd
@@ -4,3 +4,6 @@ func _ready():
 	$AudioStreamPlayer2D.play()
 func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://Menu/Scenes/menu_start.tscn")
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Credits/Creditis.tscn")
